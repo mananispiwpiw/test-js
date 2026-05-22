68bc350cb65c01f12bcba13bebd10a3b0ba7c1c9
mananispiwpiw <rafijaeger@gmail.com>
2026-05-22 20:03:09 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 export default {
   async fetch(request) {
-    const target = new URL(request.url).searchParams.get('target') || 'http://127.0.0.1:80/'
+    const url = new URL(request.url)
+    const target = url.searchParams.get('target') || 'http://127.0.0.1:80/'
+    const body = await request.text()
+    console.log('BODY:', body)
     return Response.redirect(target, 302)
   }
 }
